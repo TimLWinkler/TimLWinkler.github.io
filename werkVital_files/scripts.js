3366cdd53549f11eadca8f0f821b79ba315e9e8d
TimLWinkler <110042852+TimLWinkler@users.noreply.github.com>
2022-07-26 16:24:23 +0200

    Test

---
+++ werkVital_files/scripts.js
@@ -0,0 +1,8 @@
+/*
+    This is the place where you can do all crazy sort of things with JavaScript
+    Like defining functions jada jada jada...
+ */
+
+function clickLogo() {
+    window.location.href = "./werkVital.html";
+}
